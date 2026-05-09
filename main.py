6ab32b6050d92c18e80506c781a502617d416651
humna fatima <humna.fatima@student.howest.be>
2026-05-09 10:16:08 +0200

    incomplete file

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def read_root():
     return {"message" : "Hello World"} 
 
 from fastapi import FastAPI
-from routers import your_router
+from routers import baby
 from database import engine, Base
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.include_router(your_router.router)
+app.include_router(baby.router)
